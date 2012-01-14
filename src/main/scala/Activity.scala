67b19887e84c6e4e2cab6f2a8f02fe499d68e1ec
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2012-01-14 17:35:48 +0900

    custom AlertDialog for adding/editing fudalist

--- src/main/scala/Activity.scala
+++ src/main/scala/Activity.scala
@@ -1,26 +1,56 @@
 package tami.pen.wasuramoti
 
-import _root_.android.app.Activity
+import _root_.android.app.{Activity,AlertDialog}
 import _root_.android.preference.{PreferenceActivity,DialogPreference}
 import _root_.android.os.Bundle
-import _root_.android.widget.TextView
+import _root_.android.widget.{ArrayAdapter,Spinner}
 import _root_.android.util.{Log,AttributeSet}
-import _root_.android.view.{View,Menu,MenuItem}
+import _root_.android.view.{View,Menu,MenuItem,ViewGroup}
 import _root_.android.media.{AudioManager,AudioFormat,AudioTrack}
-import _root_.android.content.{Intent,Context}
+import _root_.android.content.{Intent,Context,DialogInterface}
 
 import _root_.mita.nep.audio.OggVorbisDecoder
 import _root_.java.io.{File,FileInputStream,FileOutputStream}
+import _root_.java.util.ArrayList
 
 
 class FudaListPreference(context:Context,attrs:AttributeSet) extends DialogPreference(context,attrs){
+  var listItems = new ArrayList[String]()
+  var adapter = None:Option[ArrayAdapter[String]]
   def this(context:Context,attrs:AttributeSet,def_style:Int) = this(context,attrs)
+  override def onBindDialogView(view:View){
+    super.onBindDialogView(view)
+    adapter = Some(new ArrayAdapter[String](context,android.R.layout.simple_spinner_item,listItems))
+    val vw = view.findViewById(R.id.fudaset_list).asInstanceOf[Spinner]
+    println(vw)
+    vw.setAdapter(adapter.get)
+    listItems.add("hoobar")
+    listItems.add("baz")
+    listItems.add("hoge")
+    listItems.add("fuga")
+    adapter.get.notifyDataSetChanged()
+  }
 }
 
-class FudaConfActivity extends PreferenceActivity {
+class FudaConfActivity extends PreferenceActivity{
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     addPreferencesFromResource(R.xml.fudaconf)
+  }
+  def addFudaList(view:View){
+    val vw = view.getRootView().findViewById(R.id.fudaset_list).asInstanceOf[Spinner]
+    val ada = vw.getAdapter().asInstanceOf[ArrayAdapter[String]]
+    val builder = new AlertDialog.Builder(this)
+    builder.setCancelable(true)
+    builder.setPositiveButton("OK",new DialogInterface.OnClickListener(){
+      override def onClick(interface:DialogInterface,which:Int){
+      }
+      })
+    val v = getLayoutInflater().inflate(R.layout.fuda_list_edit,null)
+    builder.setView(v)
+    builder.setMessage("hogefuga")
+    val dialog = builder.create()
+    dialog.show()
   }
 }
 
@@ -28,7 +58,7 @@ class WasuramotiActivity extends Activity {
   override def onCreateOptionsMenu(menu: Menu) : Boolean = {
     super.onCreateOptionsMenu(menu)
     val inflater = getMenuInflater()
-    inflater.inflate(R.menu.mainmenu, menu)
+    inflater.inflate(R.menu.main, menu)
     return true
   }
   override def onOptionsItemSelected(item: MenuItem) : Boolean = {
@@ -44,7 +74,7 @@ class WasuramotiActivity extends Activity {
   }
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.mainlayout)
+    setContentView(R.layout.main)
     val read_button = findViewById(R.id.read_button)
     read_button.setOnClickListener(new View.OnClickListener() {
       override def onClick(v: View) {
