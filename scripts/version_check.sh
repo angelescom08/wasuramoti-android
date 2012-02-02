96eabfe19a2ead0a6f8d16b29f59c3d61541f8d9
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2012-02-03 01:32:55 +0900

    prepare for version 0.3

--- scripts/version_check.sh
+++ scripts/version_check.sh
@@ -2,7 +2,9 @@
 ROOT=$(hg root)
 tput smul; echo ${ROOT}/README; tput rmul
 grep 'App Version' ${ROOT}/README | sed -e 's/^ */  /'
-tput smul; echo ${ROOT}/AndroidManifest.xml; tput rmul
+tput smul; echo ${ROOT}/src/main/AndroidManifest.xml; tput rmul
 grep -o 'android:version[A-Za-z]*="[0-9.]*"' ${ROOT}/src/main/AndroidManifest.xml | sed -e 's/^/  /'
 tput smul; echo ${ROOT}/project/build.scala; tput rmul
 grep '^ *version *:= *' ${ROOT}/project/build.scala | sed -e 's/^ */  /'
+echo
+tput bold;echo 'dont forget to execute "jarsigner" and "zipalign" to *.apk package';tput sgr0
