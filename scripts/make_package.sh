744a5536320fcc4d9b801afea1abaccdb4b66a72
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2012-02-05 18:09:17 +0900

    prepare for version 0.5.1

---
+++ scripts/make_package.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+ROOT="$(hg root)"
+KEYSTORE="${ROOT}/keystore/hpnpwd.keystore"
+READER_DIR="${ROOT}/keystore/"
+READER_ASSETS="${ROOT}/src/main/assets/reader"
+TARGET_BASE="${ROOT}"
+
+if [ $# -eq 0 ];then
+  echo "USAGE: $0 [reader]"
+  exit
+fi
+if [ ! -d "${READER_DIR}/$1" ];then
+  echo directory "${READER_DIR}/$1" not found
+  exit
+fi
+
+for f in "${READER_ASSETS}"/*; do
+  if [ -h "${f}" ];then
+    unlink "${f}"
+  fi
+done
+ln -s "${READER_DIR}"/$1 "${READER_ASSETS}"
+cd ${ROOT}
+
+sbt android:package-release
+
+APK_FILE=$(ls ${ROOT}/target/wasuramoti-*.apk)
+if [ $? -ne 0 ];then
+  exit
+fi
+READER=$(unzip -t ${APK_FILE} | grep -o -m 1 'testing: assets/reader/[^/]*/' | sed -re 's!.*/([^/]*)/$!\1!')
+
+if [ ! "${READER}" ];then
+  echo no assets/reader found in ${APK_FILE}
+  exit
+fi
+
+VERSION=$(echo "${APK_FILE}" | sed -re 's!.*/wasuramoti-(.*).apk!\1!')
+TARGET_DIR="${TARGET_BASE}/wasuramoti-android-${READER}-${VERSION}"
+mkdir "${TARGET_DIR}"
+jarsigner -verbose -keystore "${KEYSTORE}" "${APK_FILE}" techkey
+TARGET_FILE="${TARGET_DIR}/wasuramoti-${READER}-${VERSION}.apk"
+zipalign -v 4 "${APK_FILE}" "${TARGET_FILE}"
+echo "${TARGET_FILE} created"
+cp "${ROOT}/README" "${TARGET_DIR}"
+cp "${ROOT}/src/main/jni/libvorbis-"*"/COPYING" "${TARGET_DIR}"
