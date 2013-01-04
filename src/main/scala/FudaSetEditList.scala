7cace51964c4c343260c938583035361f1634d6d
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2013-01-04 23:21:59 +0900

    able to switch to fudanum order in fudaset edit list

--- src/main/scala/FudaSetEditList.scala
+++ src/main/scala/FudaSetEditList.scala
@@ -6,23 +6,61 @@ import _root_.android.content.Context
 import _root_.android.view.View
 import _root_.android.text.Html
 import _root_.android.widget.{ArrayAdapter,AdapterView,ListView,TextView,Button}
+import _root_.java.util.Comparator
 
 class FudaSetEditListDialog(context:Context,kimarijis:String,onOk:String=>Unit) extends Dialog(context,android.R.style.Theme_Black_NoTitleBar_Fullscreen){
+  object SortMode extends Enumeration {
+    type SortMode = Value
+    val ABC,NUM = Value
+  }
+  var sort_mode = SortMode.ABC
 
   class FudaListItem(val str:String, val fudanum:Int) {
-    override def toString():String = Romanization.jap_to_local(context,str)
-  }
-  
-  def addItemsToListView(container_view:ListView){
-    val have_to_read = AllFuda.makeHaveToRead(kimarijis)
-    val fudalist = AllFuda.list.zipWithIndex.sortBy{ _._1 }.map{ _ match { case (s,i) => new FudaListItem(s,i+1)} }
-    val adapter = new ArrayAdapter[FudaListItem](context,android.R.layout.simple_list_item_multiple_choice,fudalist)
-    container_view.setAdapter(adapter)
-    for( (s,pos) <- fudalist.zipWithIndex ){
-      if(have_to_read.contains(s.str)){
-        container_view.setItemChecked(pos,true)
+    override def toString():String = {
+      var r = (sort_mode match{
+        case SortMode.ABC => ""
+        case SortMode.NUM => fudanum + ". "}) + 
+        Romanization.jap_to_local(context,str)
+      if(sort_mode == SortMode.NUM && Romanization.is_japanese(context)){
+        r += " (" + AllFuda.author(fudanum-1)  + ")"
+      }
+      return r
+    }
+    def compareTo(that:FudaListItem):Int = {
+      sort_mode match{
+        case SortMode.ABC => str.compareTo(that.str)
+        case SortMode.NUM => fudanum.compareTo(that.fudanum)
       }
     }
+    def equals(that:FudaListItem):Boolean = {
+      str.equals(that.str) && fudanum.equals(that.fudanum)
+    }
+  }
+  val fudalistitem_comp = new Comparator[FudaListItem](){
+    override def compare(lhs:FudaListItem,rhs:FudaListItem):Int = {
+      lhs.compareTo(rhs)
+    }
+    override def equals(obj:Any):Boolean = {
+      this.equals(obj)
+    }
+  }
+  def checkListBy(f:FudaListItem=>Boolean){
+    val container_view = findViewById(R.id.fudaseteditlist_container).asInstanceOf[ListView]
+    val adapter = container_view.getAdapter().asInstanceOf[ArrayAdapter[FudaListItem]]
+    for( pos <- 0 until container_view.getCount ){
+      val r = f(adapter.getItem(pos))
+      container_view.setItemChecked(pos,r)
+    }
+  }
+
+  def addItemsToListView(){
+    val container_view = findViewById(R.id.fudaseteditlist_container).asInstanceOf[ListView]
+    val have_to_read = AllFuda.makeHaveToRead(kimarijis)
+    val fudalist = AllFuda.list.zipWithIndex.map{ x => new FudaListItem(x._1,x._2+1) }
+    val adapter = new ArrayAdapter[FudaListItem](context,android.R.layout.simple_list_item_multiple_choice,fudalist)
+    adapter.sort(fudalistitem_comp)
+    container_view.setAdapter(adapter)
+    checkListBy(x=>have_to_read.contains(x.str))
   }
 
   override def onCreate(bundle:Bundle){
@@ -33,7 +71,7 @@ class FudaSetEditListDialog(context:Context,kimarijis:String,onOk:String=>Unit) 
       Html.fromHtml(context.getString(R.string.button_invert))
     )
     val container_view = findViewById(R.id.fudaseteditlist_container).asInstanceOf[ListView]
-    addItemsToListView(container_view)
+    addItemsToListView()
     val get_num_list = ()=> {
       val poss = container_view.getCheckedItemPositions()
       val adapter = container_view.getAdapter().asInstanceOf[ArrayAdapter[FudaListItem]]
@@ -58,6 +96,19 @@ class FudaSetEditListDialog(context:Context,kimarijis:String,onOk:String=>Unit) 
           container_view.setItemChecked(pos,!q)
         }
         update_fudanum()
+      }
+    })
+    findViewById(R.id.button_invert).setOnLongClickListener(new View.OnLongClickListener(){
+      override def onLongClick(v:View):Boolean = {
+        sort_mode = if(sort_mode == SortMode.NUM){SortMode.ABC}else{SortMode.NUM}
+        val adapter = container_view.getAdapter().asInstanceOf[ArrayAdapter[FudaListItem]]
+        val num_list = get_num_list().toSet
+        adapter.sort(fudalistitem_comp)
+        adapter.notifyDataSetChanged()
+        // I could not find a way to preserve checkbox state after sort() is called.
+        // Therefore I do it by hand.
+        checkListBy(x=>num_list.contains(x.fudanum))
+        return(true)
       }
     })
     findViewById(R.id.button_cancel).setOnClickListener(new View.OnClickListener(){
