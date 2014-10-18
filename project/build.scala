e173583cd1d70545ba1e56294cfb3a41dd344070
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2014-10-18 17:10:40 +0900

    prepare for version 0.8.13

--- project/build.scala
+++ project/build.scala
@@ -6,8 +6,8 @@ import sbtandroid.AndroidPlugin._
 object AndroidBuild extends Build{
   lazy val globalSettings = Seq (
     name := "Wasuramoti",
-    version := "0.8.12",
-    versionCode := 50,
+    version := "0.8.13",
+    versionCode := 51,
     scalaVersion := "2.10.2",
     platformName := "android-18",
     keyalias := "techkey",
