84b6c7ff3c4cc07339e8e361fb4cb89eb5fe8d3f
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2015-03-24 12:53:50 +0900

    bump to version 0.8.21

--- project/build.scala
+++ project/build.scala
@@ -4,8 +4,8 @@ import android.Keys._
 object Build extends android.AutoBuild {
   lazy val mySettings = super.settings ++ android.Plugin.androidBuild ++ Seq (
     name := "wasuramoti",
-    version := "0.8.20",
-    versionCode := Some(58),
+    version := "0.8.21",
+    versionCode := Some(59),
     scalaVersion := "2.11.5",
     platformTarget in Android := "android-19",
     buildToolsVersion in Android := Some("19.1.0"),
