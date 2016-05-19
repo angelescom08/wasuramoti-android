4097b2d2ac24c2ab43b5dad334b4c28f5bdf80c7
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2016-05-19 12:08:00 +0900

    change button text color when drag started

--- src/main/scala/FudaSetReOrderDialog.scala
+++ src/main/scala/FudaSetReOrderDialog.scala
@@ -13,8 +13,11 @@ class FudaSetReOrderDialog(
   context:Context
   ) extends AlertDialog(context) with CustomAlertDialogTrait{
 
-  val ACTIVE_COLOR = Color.argb(255,0xFF,0xFF,0x00)
-  val DEFAULT_COLOR = Color.argb(255,0x44,0x44,0x44)
+  val BAR_COLOR_ACTIVE = Color.CYAN
+  val BAR_COLOR_DEFAULT = Color.argb(255,0x44,0x44,0x44)
+
+  val TEXT_COLOR_ACTIVE = Color.GRAY
+  var TEXT_COLOR_DEFAULT = None:Option[Int]
 
   override def doWhenClose(view:View){
     //TODO
@@ -24,15 +27,29 @@ class FudaSetReOrderDialog(
     v.findViewWithTag("horizontal_rule_droppable_body").setBackgroundColor(color)
   }
 
+  def setTextColorOrDefault(v:TextView,color:Option[Int]){
+    color match {
+      case Some(c) =>
+        if(TEXT_COLOR_DEFAULT.isEmpty){
+          TEXT_COLOR_DEFAULT = Some(v.getCurrentTextColor)
+        }
+        v.setTextColor(c)
+      case None =>
+        TEXT_COLOR_DEFAULT.foreach{ c =>
+          v.setTextColor(c)
+        }
+    }
+  }
+
   def addBorder(list:ViewGroup):View = {
     val v = LayoutInflater.from(context).inflate(R.layout.horizontal_rule_droppable, null)
     v.setOnDragListener(new View.OnDragListener{
         override def onDrag(v:View, event:DragEvent):Boolean = {
           event.getAction match {
             case DragEvent.ACTION_DRAG_ENTERED =>
-              setBorderColor(v,ACTIVE_COLOR)
+              setBorderColor(v,BAR_COLOR_ACTIVE)
             case DragEvent.ACTION_DRAG_EXITED | DragEvent.ACTION_DRAG_ENDED =>
-              setBorderColor(v,DEFAULT_COLOR)
+              setBorderColor(v,BAR_COLOR_DEFAULT)
             case _ => 
           }
           true
@@ -55,6 +72,7 @@ class FudaSetReOrderDialog(
         override def onLongClick(v:View):Boolean = {
           val data = ClipData.newPlainText("text",fs.title)
           v.startDrag(data, new View.DragShadowBuilder(v), v, 0)
+          setTextColorOrDefault(v.asInstanceOf[TextView],Some(TEXT_COLOR_ACTIVE))
           true
         }
       })
@@ -63,15 +81,17 @@ class FudaSetReOrderDialog(
           event.getAction match{
             case DragEvent.ACTION_DRAG_LOCATION =>
               if(event.getY <= v.getHeight / 2){
-                setBorderColor(borderPrev,ACTIVE_COLOR)
-                setBorderColor(borderNext,DEFAULT_COLOR)
+                setBorderColor(borderPrev,BAR_COLOR_ACTIVE)
+                setBorderColor(borderNext,BAR_COLOR_DEFAULT)
               }else{
-                setBorderColor(borderPrev,DEFAULT_COLOR)
-                setBorderColor(borderNext,ACTIVE_COLOR)
+                setBorderColor(borderPrev,BAR_COLOR_DEFAULT)
+                setBorderColor(borderNext,BAR_COLOR_ACTIVE)
               }
             case DragEvent.ACTION_DRAG_EXITED =>
-              setBorderColor(borderPrev,DEFAULT_COLOR)
-              setBorderColor(borderNext,DEFAULT_COLOR)
+              setBorderColor(borderPrev,BAR_COLOR_DEFAULT)
+              setBorderColor(borderNext,BAR_COLOR_DEFAULT)
+            case DragEvent.ACTION_DRAG_ENDED =>
+              setTextColorOrDefault(v.asInstanceOf[TextView],None)
             case _ =>
           }
           true
