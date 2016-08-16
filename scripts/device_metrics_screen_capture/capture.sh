d1ca42b63813d01091e4582c50313a8965188a0a
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2016-08-17 00:16:13 +0900

    capture both landscape and portrait screenshot

--- scripts/device_metrics_screen_capture/capture.sh
+++ scripts/device_metrics_screen_capture/capture.sh
@@ -21,6 +21,10 @@ if ! [ -e "$CAPTURE_PATH" ];then
   mkdir "$CAPTURE_PATH" || exit 1
 fi
 
+# disable accelerometer controlling rotation
+adb_shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0 || exit 1
+
+
 jq -r < device_metrics.json '[.title,(.width|tostring),(.height|tostring),.density]|join("\t")' | \
 while IFS=$'\t' read title width height density; do
   id=$(normalize_title "$title")
@@ -32,14 +36,23 @@ while IFS=$'\t' read title width height density; do
     ww=$width
     hh=$height
   fi
-  adb_shell wm size "$ww"x"$hh" || exit 1
+  adb_shell wm size "$ww"x"$hh"
   adb_shell wm density "$dpi"
   adb_shell am force-stop karuta.hpnpwd.wasuramoti
   adb_shell am start -a android.intent.action.MAIN -n karuta.hpnpwd.wasuramoti/.WasuramotiActivity
-  adb_shell dumpsys window | grep 'mCurConfiguration' > $BASE_PATH/capture/"$id".info
-  image=/sdcard/"$id".png
-  sleep 2
-  adb_shell screencap -p "$image"
-  adb -e pull "$image" "$CAPTURE_PATH"/"$id".png 
-  adb_shell rm "$image"
+  for rotation in port land; do
+    prefix="$id"-$rotation
+    image=/sdcard/"$prefix".png
+    # rotate device
+    if [ $rotation = port ];then
+      adb_shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:0
+    else
+      adb_shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:1
+    fi
+    sleep 2
+    adb_shell screencap -p "$image"
+    adb_shell dumpsys window | grep 'mCurConfiguration' > $BASE_PATH/capture/"$prefix".info
+    adb -e pull "$image" "$CAPTURE_PATH"/"$prefix".png 
+    adb_shell rm "$image"
+  done
 done
