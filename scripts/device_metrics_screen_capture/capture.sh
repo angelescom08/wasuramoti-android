7e3eac4d6ca4168ac589a72a4c62e4c072a821cd
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2016-08-17 11:13:01 +0900

    check that requested size/density is same as actual size

--- scripts/device_metrics_screen_capture/capture.sh
+++ scripts/device_metrics_screen_capture/capture.sh
@@ -17,6 +17,52 @@ function density_to_dpi(){
   bc <<< "(160*$1)/1"
 }
 
+function resize_or_error(){
+  local sz="$1"
+  adb_shell wm size "$sz"
+  local actual physical
+  while read p _ s; do
+    case "$p" in
+      Physical)
+        physical="$s";;
+      Override)
+        actual="$s";;
+    esac
+  done < <(adb_shell wm size | tr -d $'\r')
+  if ! [ "$actual" ];then
+    actual="$physical"
+  fi
+  if [ "$actual" != "$sz" ];then
+    echo "Requested size does not match actual override size: $sz != $actual"
+    echo "Check that emulator as enough physical size."
+    echo "The maximum override size is twice of physical size."
+    exit 1
+  fi
+}
+
+function density_or_error(){
+  local density="$1"
+  local dpi=$(density_to_dpi "$density")
+  adb_shell wm density "$dpi"
+  local actual physical
+  while read p _ s; do
+    case "$p" in
+      Physical)
+        physical="$s";;
+      Override)
+        actual="$s";;
+    esac
+  done < <(adb_shell wm density | tr -d $'\r')
+  if ! [ "$actual" ];then
+    actual="$physical"
+  fi
+  if [ "$actual" != "$dpi" ];then
+    echo "Requested density does not match actual override density: $dpi != $actual"
+    echo "Cannot set density."
+    exit 1
+  fi
+}
+
 if ! [ -e "$CAPTURE_PATH" ];then
   mkdir "$CAPTURE_PATH" || exit 1
 fi
@@ -28,7 +74,6 @@ adb_shell content insert --uri content://settings/system --bind name:s:accelerom
 jq -r < device_metrics.json '[.title,(.width|tostring),(.height|tostring),.density]|join("\t")' | \
 while IFS=$'\t' read title width height density; do
   id=$(normalize_title "$title")
-  dpi=$(density_to_dpi "$density")
   if ((width > height));then
     ww=$height
     hh=$width
@@ -36,8 +81,9 @@ while IFS=$'\t' read title width height density; do
     ww=$width
     hh=$height
   fi
-  adb_shell wm size "$ww"x"$hh"
-  adb_shell wm density "$dpi"
+  resize_or_error "${ww}x${hh}"
+  density_or_error "$density"
+
   adb_shell am force-stop karuta.hpnpwd.wasuramoti
   adb_shell am start -a android.intent.action.MAIN -n karuta.hpnpwd.wasuramoti/.WasuramotiActivity
   for rotation in port land; do
