cb0ea1dd3a550831af1419324a3df28a1438c3b8
Haruhiro Yoshimoto <pjmtdw@gmail.com>
2017-03-20 18:17:23 +0900

    add landscape layout for fudasetedit_num

--- src/main/scala/FudaSetEditNum.scala
+++ src/main/scala/FudaSetEditNum.scala
@@ -1,13 +1,13 @@
 package karuta.hpnpwd.wasuramoti
 
-import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.widget.{LinearLayout,ToggleButton,ListView,CompoundButton,RadioGroup,ArrayAdapter}
 import android.view.{LayoutInflater,ViewGroup,View}
 import scala.collection.mutable
 
-class FudaSetEditNumDialog(context:Context,callback:(Set[Int])=>Unit) extends AlertDialog(context) with CustomAlertDialogTrait{
+class FudaSetEditNumDialog(context:Context,callback:(Set[Int])=>Unit) extends Dialog(context,android.R.style.Theme_Black_NoTitleBar_Fullscreen){
 
   val TAG_NUM = R.id.fudasetedit_tag_num
 
@@ -34,18 +34,14 @@ class FudaSetEditNumDialog(context:Context,callback:(Set[Int])=>Unit) extends Al
     search(container)
   }
 
-  override def doWhenClose(view:View){
-    val list_view = findViewById(R.id.fudaseteditnum_container).asInstanceOf[ListView]
-    callback(Utils.getCheckedItemsFromListView[FudaListItem](list_view).map(_.num).toSet)
-  }
-
   override def onCreate(bundle:Bundle){
+    super.onCreate(bundle)
+    setContentView(R.layout.fudasetedit_num)
     setTitle(R.string.fudasetedit_num_title)
-    val root = LayoutInflater.from(context).inflate(R.layout.fudasetedit_num,null)
-    val container = root.findViewById(R.id.fudasetedit_num_list).asInstanceOf[LinearLayout]
+    val container = findViewById(R.id.fudasetedit_num_list).asInstanceOf[LinearLayout]
     val sar = context.getResources.getStringArray(R.array.fudasetedit_numbers)
-    val list_view = root.findViewById(R.id.fudaseteditnum_container).asInstanceOf[ListView]
-    val radio_group = root.findViewById(R.id.fudasetedit_num_type).asInstanceOf[RadioGroup]
+    val list_view = findViewById(R.id.fudaseteditnum_container).asInstanceOf[ListView]
+    val radio_group = findViewById(R.id.fudasetedit_num_type).asInstanceOf[RadioGroup]
     radio_group.check(R.id.fudasetedit_num_type_ones_digit)
     val filtered = (n:Int)=>radio_group.getCheckedRadioButtonId match{
       case R.id.fudasetedit_num_type_ones_digit =>
@@ -86,7 +82,17 @@ class FudaSetEditNumDialog(context:Context,callback:(Set[Int])=>Unit) extends Al
       list_view.setAdapter(adapter)
       container.addView(row)
     }
-    setViewAndButton(root)
-    super.onCreate(bundle)
+    findViewById(R.id.button_cancel).setOnClickListener(new View.OnClickListener(){
+      override def onClick(v:View){
+        dismiss()
+      }
+    })
+    findViewById(R.id.button_ok).setOnClickListener(new View.OnClickListener(){
+      override def onClick(v:View){
+        val list_view = findViewById(R.id.fudaseteditnum_container).asInstanceOf[ListView]
+        callback(Utils.getCheckedItemsFromListView[FudaListItem](list_view).map(_.num).toSet)
+        dismiss()
+      }
+    })
   }
 }
