5309ed00a466e04f761815b501b8f599d00a9cfd
Haruhiro Yoshimoto <pjmtdw@gmail.com>
2017-10-20 21:49:53 +0900

    reload UI when show_rewind_button has been changed

--- src/main/scala/PrefActivity.scala
+++ src/main/scala/PrefActivity.scala
@@ -100,7 +100,7 @@ class PrefFragment extends PreferenceFragmentCompat
         "wav_begin_read"|"wav_end_read"|"wav_span_simokami"|"wav_threshold"|
         "wav_fadeout_simo"|"wav_fadein_kami"|"fudaset" =>
         Globals.forceRefreshPlayer = true
-      case "show_replay_last_button" | "show_skip_button" =>
+      case "show_replay_last_button" | "show_skip_button" | "show_rewind_button"=>
         Globals.forceReloadUI = true
       case "read_order_each" =>
         // we also have to change text of replay_last_button when read_order_each changed
