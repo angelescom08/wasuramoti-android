fbeca7bc8d6d495bb7e76e8fcd48e1524b26a892
Haruhiro Yoshimoto <pjmtdw@gmail.com>
2018-09-24 00:06:08 +0900

    completed autumn color theme

--- src/main/scala/ColorThemePref.scala
+++ src/main/scala/ColorThemePref.scala
@@ -76,7 +76,7 @@ object ColorThemeHelper {
     ColorTheme("white",true,false,R.id.color_theme_white,R.string.color_theme_white,R.style.Wasuramoti_MainTheme_White,R.style.Wasuramoti_PrefTheme_White),
     ColorTheme("spring",true,true,R.id.color_theme_spring,R.string.color_theme_spring,R.style.Wasuramoti_MainTheme_Spring,R.style.Wasuramoti_PrefTheme_Spring),
     ColorTheme("summer",true,true,R.id.color_theme_summer,R.string.color_theme_summer,R.style.Wasuramoti_MainTheme_Summer,R.style.Wasuramoti_PrefTheme_Summer),
-    ColorTheme("autumn",false,true,R.id.color_theme_autumn,R.string.color_theme_autumn,R.style.Wasuramoti_MainTheme_Autumn,R.style.Wasuramoti_PrefTheme_Autumn),
+    ColorTheme("autumn",true,true,R.id.color_theme_autumn,R.string.color_theme_autumn,R.style.Wasuramoti_MainTheme_Autumn,R.style.Wasuramoti_PrefTheme_Autumn),
     ColorTheme("winter",false,true,R.id.color_theme_winter,R.string.color_theme_winter,R.style.Wasuramoti_MainTheme_Winter,R.style.Wasuramoti_PrefTheme_Winter)
     )
   val themesMap = themes.map{t => (t.tag,t)}.toMap
