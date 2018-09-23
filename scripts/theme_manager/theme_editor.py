b663e30a6d23af522fb3bfec294553effbce2090
Haruhiro Yoshimoto <pjmtdw@gmail.com>
2018-09-23 15:58:42 +0900

    able to run theme_editor.py from another path

--- scripts/theme_manager/theme_editor.py
+++ scripts/theme_manager/theme_editor.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
+from subprocess import check_output
 from pathlib import Path
 import yaml
 from flask import Flask, render_template, request
 
-THEME_DIR = './theme/'
-app = Flask(__name__, template_folder='assets')
+ROOT_DIR = str(check_output("git rev-parse --show-toplevel", shell=True), 'utf-8').rstrip()
+CUR_DIR = Path(__file__).parent
+THEME_DIR = CUR_DIR / 'theme'
+TEMPLATE_FOLDER = (CUR_DIR/'assets').resolve()
+app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
 
 @app.route('/')
 def get_index():
   html = '<!doctype html><title>Wasuramoti Theme Editor</title>'
-  for yml in Path(THEME_DIR).glob('*.yml'):
+  for yml in THEME_DIR.glob('*.yml'):
     tag = yml.stem
     html += '<div><a href="/theme/{}">[{}]</a></div>'.format(tag, tag)
   return html
@@ -25,13 +29,13 @@ def save_theme(tag):
   config = load_config(tag)
   for name, color in request.form.items():
     config['colors'][name] = color
-  with open(Path(THEME_DIR) / '{}_new.yml'.format(tag), 'w') as fout:
+  with open(THEME_DIR / '{}_new.yml'.format(tag), 'w') as fout:
     yaml.dump(config, fout, default_flow_style=False)
   return 'ok'
 
 
 def load_config(tag):
-  with open(Path(THEME_DIR) / '{}.yml'.format(tag)) as fin:
+  with open(THEME_DIR / '{}.yml'.format(tag)) as fin:
     return yaml.load(fin)
 
 if __name__ == '__main__':
